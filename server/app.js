48695f7f5e269281ff507ccec036f08083485f28
CodeWithRajMishra <rajmishra3@gmail.com>
2025-02-20 16:55:08 +0530

    pro

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 // Parse incoming requests with urlencoded payloads
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://127.0.0.1:27017/harshirelationship").then(()=>{
+mongoose.connect("mongodb+srv://rajmishra3:X90jYWUvxLOos8d5@cluster0.9ha67.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
     console.log("DB connected!!!");
 })
 
